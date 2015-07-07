fcdbeb600d1b8b910e779241d2e4b7676d198718
Johan Swensson <kupo@kupo.se>
2015-07-07 23:07:36 +0200

    minor typos

--- pulp.pp
+++ pulp.pp
@@ -1,16 +1,16 @@
 exec { "Fetch pulp repo file":
-  command => "curl https://repos.fedorapeople.org/repos/pulp/pulp/rhel-pulp.repo > /etc/yum.repos.d/rhel-pulp.repo",
-  creates => File["/etc/yum.repos.d/rhel-pulp.repo"]
+  command => "/usr/bin/curl https://repos.fedorapeople.org/repos/pulp/pulp/rhel-pulp.repo > /etc/yum.repos.d/rhel-pulp.repo",
+  creates => "/etc/yum.repos.d/rhel-pulp.repo"
 }
 
 exec { "Enable EPEL":
-  command => "rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm",
+  command => "/usr/bin/rpm -Uvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm",
   creates => "/etc/yum.repos.d/epel.repo"
 }
 
 #python-blinker is retired in epel7
 exec { "Enable copr python-blinker":
-  command => "curl http://copr.fedoraproject.org/coprs/opuk/python-blinker/repo/epel-7/opuk-python-blinker-epel-7.repo > /etc/yum.repos.d/opuk-python-blinker-epel-7.repo",
+  command => "/usr/bin/curl http://copr.fedoraproject.org/coprs/opuk/python-blinker/repo/epel-7/opuk-python-blinker-epel-7.repo > /etc/yum.repos.d/opuk-python-blinker-epel-7.repo",
   creates => "/etc/yum.repos.d/opuk-python-blinker-epel-7.repo"
 }
 
@@ -37,7 +37,7 @@ package { "qpid-tools": ensure => present, require =>  [ Exec["Enable EPEL"], Ex
 package { "pulp-admin": ensure => present, require =>  [ Exec["Enable EPEL"], Exec["Fetch pulp repo file" ] ] } 
 
 exec { "pulp-manage-db":
-  command => "pulp-manage-db && touch /var/lib/pulp/.puppetinit",
+  command => "/usr/bin/pulp-manage-db && /usr/bin/touch /var/lib/pulp/.puppetinit",
   user     => apache,
   creates  => "/var/lib/pulp/.puppetinit",
   require  => [ Package["pulp-admin"], Package["qpid-tools"], Package["@pulp-server-qpid"] ],
