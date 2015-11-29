e35d1a006dbe8e84479d324122bdca40a9747949
Johan Swensson <kupo@kupo.se>
2015-11-29 12:47:20 +0100

    drop echo

--- scripts/update-certs/update.sh
+++ scripts/update-certs/update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $(cat repos.list); do echo pulp-admin rpm repo update --repo-id $i --feed-cert=/root/employee.crt --feed-key=/root/employee.key; done
+for i in $(cat repos.list); do pulp-admin rpm repo update --repo-id $i --feed-cert=/root/employee.crt --feed-key=/root/employee.key; done
