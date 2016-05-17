fa0a1b1cbd29fa3a8c5fc066126637861a5a5fdb
Johan Swensson <kupo@kupo.se>
2016-05-17 21:42:17 +0200

    deploy using ansible

--- runme.sh
+++ runme.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-yum install puppet -y
-puppet module install terrimonster-yumgroup
-puppet apply pulp.pp
+yum install epel-release -y
+yum install ansible -y
+ansible-playbook install.yml
 
